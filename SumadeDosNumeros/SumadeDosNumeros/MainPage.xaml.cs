c613d99acc88a672c185598dfa9e94b16e1db475
RokieTheJawa <72667052+RokieTheJawa@users.noreply.github.com>
2021-01-06 03:31:58 -0600

    Agregar archivos de proyecto.

---
+++ SumadeDosNumeros/SumadeDosNumeros/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SumadeDosNumeros
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                float a = float.Parse(valor1.Text);
+                float b = float.Parse(valor2.Text);
+                float c = a + b;
+                string v = a + "+" + b + "=" + c;
+                resultado.Text = v;
+            }
+            catch (Exception)
+            {
+
+                resultado.Text="Datos Inválidos";
+            }
+        }
+    }
+}
